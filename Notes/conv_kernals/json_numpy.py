a4167f820b92e6615357da4b276c4e327432b17d
Dan-Blanchette <blan5568@vandals.uidaho.edu>
2024-08-30 10:28:41 -0700

    json example.

---
+++ Notes/conv_kernals/json_numpy.py
@@ -0,0 +1,25 @@
+import numpy as np
+import json
+
+m = np.array([[1,5,5],[2,6,3]])
+
+id = np.identity(3, dtype=np.float16)
+
+# print(id, m)
+
+
+# print(np.ones((3,2)))
+# print(np.zeros((2,2)))
+
+# print(np.arange(0,100).reshape(2,50))
+# print(dir(m))
+
+# print(m.mean())
+
+
+output_object = {
+   'matrix' : m.tolist()
+}
+
+with open('output.json', 'w') as fp :
+   json.dump(output_object, fp)
